311022e2f924a66157054985a64acad20ce19d35
Vasyl Myronchuk <granfalloner@gmail.com>
2023-11-25 10:21:02 +0100

    Init hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config()
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    baobab: {
+      url: process.env.KLAYTN_BAOBAB_URL || "",
+      gasPrice: 250000000000,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  }
+};
+
+export default config;
